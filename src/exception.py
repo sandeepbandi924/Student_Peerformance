3238dc4784c8b981e09372c6233e30491311cfa2
Sandeep Bandi <bandisandeep1374@gmail.com>
2024-10-04 14:19:21 +0530

    logger and exception

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+from src.logger import logging
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message = f'Error Occured in {[file_name]},Line Number {[exc_tb.tb_lineno]} error Message {[str(error)]}'
+    return error_message
+
+class CustomeException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self) -> str:
+        return self.error_message
+
+
+if __name__=='__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Division Error')
+        raise CustomeException(e,sys)
+        
